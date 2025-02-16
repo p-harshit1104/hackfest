a77948c3c9b1c3b1b228a8595603a2e92df9551c
p-harshit1104 <harshitp110604@gmail.com>
2025-02-16 21:55:47 +0530

    he

---
+++ day1.js
@@ -0,0 +1,15 @@
+
+let a=1;
+let b=true;
+
+
+
+//loose equality
+if (a == b) {
+    console.log("a and b are equal");
+}
+
+//strict equality
+if (a === b) {
+    console.log("a and b are strictly equal");
+}
