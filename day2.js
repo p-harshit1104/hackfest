4b5864c76fe07a0a9e4258730f2d5d91ae58c1ac
p-harshit1104 <harshitp110604@gmail.com>
2025-02-17 14:51:44 +0530

    h

---
+++ day2.js
@@ -0,0 +1,70 @@
+//******************************function declaration********************************************
+
+// print("riya")
+
+// function print(a){
+//     console.log("hello ",a);
+// }
+
+// print("harshit");
+
+//1)can be defined several times with same name
+//2)we can call it before declaration
+
+
+//******************************function assignment********************************************
+
+
+// const view=function printtext(a) {
+//     console.log("hi ",a)
+
+// }
+
+// view("hero")
+// printtext("hero")
+
+//******************************anoymous function assignment********************************************
+
+// const view=function (a) {
+//     console.log("hi ",a)
+
+// }
+
+// view("hero")
+// printtext("hero")
+
+//******************************arrow function assignment (ES6 syntax)********************************************
+
+
+// const view=(a)=> {
+//     console.log("hi ",a)
+
+// }
+
+// view("hero")
+// printtext("hero")
+
+
+//******************************wap for calculating sum and return its mod by all types********************************************
+
+
+
+// function sum(a,b) {
+//     return (a+b)%10;                    //type 1
+// }
+
+// const fun=function sum(a,b) {
+//     return (a+b)%10;                    //TYPE2
+// }
+
+
+// const fun=function(a,b){
+//     return (a+b)%10;                //type 3
+// }
+
+// const fun= (a,b)=>{                  
+//     return (a+b)%10;                 //type 4
+// }
+
+
+console.log(fun(10,5));
